6364d1431ee925212a44c83ae1c93aff9432dc3c
DashinIN <green19grant@yandex.ru>
2022-02-18 15:43:34 +0300

    снег

--- js/script.js
+++ js/script.js
@@ -1,4 +1,46 @@
 
+const isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+
+    Windows: function() {
+     return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows()); 
+        
+    }
+}
+
+
+const Main = document.querySelector(".main");
+const Articles = document.querySelector(".articles");
+
+
+if(isMobile.any()) {
+  
+    Main.classList.add("_pc");
+    Articles.classList.add("_pc");
+
+}
+
+
 
 
 //Открывает - закрывает меню
